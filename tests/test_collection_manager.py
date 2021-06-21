594f654298b7a17a434d3df2669aeaeb50fa0e8d
daki <drakosam@gmail.com>
2021-06-21 12:25:23 +0200

    tags in collection manager

--- tests/test_collection_manager.py
+++ tests/test_collection_manager.py
@@ -55,10 +55,12 @@ def test_collection_manager_add_new_collection_item_only_if_is_valid():
     assert result_3
     assert not result_4
 
+
 def test_collection_manager_get_image_retur_empty_string_when_collections_are_empty():
     test_item = CollectionManager()
 
     assert test_item.get_image() == ''
+
 
 def test_collection_manager_get_image_retur_by_defalut_first_image_of_first_collection():
     test_item = CollectionManager()
@@ -124,6 +126,7 @@ def test_collection_manager_get_image_overflow_pick_image_in_collection_by_index
 
     assert test_item.get_image(image_index=100,collection_index=1) == ''
 
+
 def test_collection_manager_get_image_overflow_pick_collection_by_index_return_empty_string():
     test_item = CollectionManager()
 
@@ -135,6 +138,7 @@ def test_collection_manager_get_image_overflow_pick_collection_by_index_return_e
     test_item.add_collection(collection=test_collection_3)
 
     assert test_item.get_image(image_index=1,collection_index=100) == ''
+
 
 def test_collection_manager_next_image_show_next_image_in_collection():
     test_item = CollectionManager()
@@ -152,6 +156,7 @@ def test_collection_manager_next_image_show_next_image_in_collection():
 
     assert test_result_1 == test_collection_1.get_image(0)
     assert test_result_2 == test_collection_1.get_image(1)
+
 
 def test_collection_manager_next_image_cycle():
     test_item = CollectionManager()
@@ -172,6 +177,7 @@ def test_collection_manager_next_image_cycle():
     assert test_result_1 == test_collection_1.get_image(0)
     assert test_result_2 == test_collection_1.get_image(0)
 
+
 def test_collection_manager_next_image_with_minus_step_show_prev_image():
     test_item = CollectionManager()
 
@@ -185,6 +191,7 @@ def test_collection_manager_next_image_with_minus_step_show_prev_image():
     test_item.next_image(-1)
 
     assert test_item.get_image() == test_collection_1.get_image(2)
+
 
 def test_collection_manager_next_collection_show_next_collection():
     test_item = CollectionManager()
@@ -202,6 +209,7 @@ def test_collection_manager_next_collection_show_next_collection():
 
     assert test_result_1 == test_collection_1.get_image(0)
     assert test_result_2 == test_collection_2.get_image(0)
+
 
 def test_collection_manager_next_collection_show_cycle():
     test_item = CollectionManager()
@@ -240,6 +248,7 @@ def test_collection_manager_next_collection_with_minus_step_show_prev_collection
     assert test_result_1 == test_collection_1.get_image(0)
     assert test_result_2 == test_collection_3.get_image(0)
 
+
 def test_collection_manager_next_collection_reset_image_index():
     test_item = CollectionManager()
 
@@ -258,3 +267,53 @@ def test_collection_manager_next_collection_reset_image_index():
     assert test_result_1 == test_collection_1.get_image(1)
     assert test_result_2 == test_collection_2.get_image(0)
 
+
+def test_collection_manager_get_collection_names():
+    test_item = CollectionManager()
+
+    test_collection_1 = Collection('111','path1',['img_1_1','img_1_2','img_1_3'])
+    test_collection_2 = Collection('222','path2',['img_2_1','img_2_2','img_2_3'])
+    test_collection_3 = Collection('333','path3',['img_3_1','img_3_2','img_3_3'])
+    test_item.add_collection(collection=test_collection_1)
+    test_item.add_collection(collection=test_collection_2)
+    test_item.add_collection(collection=test_collection_3)
+
+    data = test_item.get_collection_names()
+
+    assert len(data) == 3
+    print(data)
+    assert '111' in data
+    assert '222' in data
+    assert '333' in data
+
+
+def test_collection_manager_toggle_tag_filter():
+    test_item = CollectionManager()
+
+    test_collection_1 = Collection('111','path1',['img_1_1','img_1_2','img_1_3'])
+    test_collection_2 = Collection('222','path2',['img_2_1','img_2_2','img_2_3'])
+    test_collection_3 = Collection('333','path3',['img_3_1','img_3_2','img_3_3'])
+    test_item.add_collection(collection=test_collection_1)
+    test_item.add_collection(collection=test_collection_2)
+    test_item.add_collection(collection=test_collection_3)
+
+    test_item.toggle_tag_in_collection(0,'tag1')
+    test_item.toggle_tag_in_collection(1,'tag2')
+    test_item.toggle_tag_in_collection(2,'tag2')
+
+    test_item.toggle_tag_filter('tag1')
+    result_1 = test_item.get_collection_names()
+    test_item.toggle_tag_filter('tag2')
+    result_2 = test_item.get_collection_names()
+    test_item.toggle_tag_filter('tag1')
+    result_3 = test_item.get_collection_names()
+    
+    assert len(result_1) == 1
+    assert '111' in result_1
+    assert len(result_2) == 3
+    assert '111' in result_2
+    assert '222' in result_2
+    assert '333' in result_2
+    assert len(result_3) == 2
+    assert '222' in result_3
+    assert '333' in result_3
--- utility/collection_manager.py
+++ utility/collection_manager.py
@@ -13,26 +13,27 @@ class CollectionManager():
 
     def next_image(self, step=1):
         self.image_index += step
-        if self.image_index >= self.collections[self.collection_index].size():
+        if self.image_index >= self.show_collection[self.collection_index].size():
             self.image_index = 0
         elif self.image_index < 0:
-            self.image_index = self.collections[self.collection_index].size() - 1
+            self.image_index = self.show_collection[self.collection_index].size() - 1
     
     def next_collection(self, step=1):
         self.collection_index += step
-        if self.collection_index >= len(self.collections):
+        if self.collection_index >= len(self.show_collection):
             self.collection_index = 0
         elif self.collection_index < 0:
-            self.collection_index = len(self.collections) - 1
+            self.collection_index = len(self.show_collection) - 1
         self.image_index = 0
 
     def size(self):
-        return len(self.collections)
+        return len(self.show_collection)
 
     def add_collection(self, name = '', path = '', images: List[str] = [], collection:Collection = None )-> bool:
         
         if collection is not None and collection.valid():
             self.collections.append(collection)
+            self.show_collection = self._get_collection_to_show()
             return True
         elif collection is not None:
             return False
@@ -45,10 +46,12 @@ class CollectionManager():
 
         self.collections.append(Collection(name,path,images))
 
+        self.show_collection = self._get_collection_to_show()
+
         return True
 
     def get_image(self , collection_index=-1, image_index=-1):
-        if len(self.collections) == 0 or not collection_index < len(self.collections):
+        if len(self.show_collection) == 0 or not collection_index < len(self.show_collection):
             return ''
 
         if collection_index > -1:
@@ -57,10 +60,36 @@ class CollectionManager():
         if image_index > -1:
             self.image_index = image_index
 
-        return self.collections[self.collection_index].get_image(self.image_index)
+        return self.show_collection[self.collection_index].get_image(self.image_index)
 
     def get_collection_names(self)->List[str]:
-        pass
+        name_list = []
+        for it in self.show_collection:
+            name_list.append(it.get_name())
+        return name_list
 
-    def toggle_tag_filter(self):
-        pass
+    def toggle_tag_in_collection(self, index, tag):
+        if index < len(self.show_collection):
+            self.show_collection[index].toggle_tag(tag)
+
+    def _get_collection_to_show(self):
+        if len(self.filter_tags) == 0:
+            return self.collections
+        
+        self.show_collection = []
+        for item in self.collections:
+            in_collection = False
+            for tag in self.filter_tags:
+                in_collection = in_collection or item.tag_in_collection(tag)
+            
+            if in_collection:
+                self.show_collection.append(item)
+
+        return self.show_collection
+
+    def toggle_tag_filter(self, tag):
+        if tag in self.filter_tags:
+            self.filter_tags.remove(tag)
+        else:
+            self.filter_tags.append(tag)
+        return self._get_collection_to_show()
